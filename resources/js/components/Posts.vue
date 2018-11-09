5899f37feece69b4b7480b8c0c65c3eb41f22de8
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-11-09 11:26:06 -0600

    :clipboard: firs commit

---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+import axios from 'axios'
+import CustomSelect from './CustomSelect.vue'
+export default {
+    components: {
+        'sellect2': CustomSelect
+    },
+    data(){
+        return {
+            posts:[]
+        }
+    },
+    mounted(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.posts = response.data
+        })
+        .catch(err => {
+
+        })
+        .finally(() => {
+
+        })
+    }
+}
+</script>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content" id="app">
+                select2
+                <div class="title m-b-md">
+                    <posts></posts>
+                </div>
+            </div>
+        </div>
+        <script
+        src="https://code.jquery.com/jquery-1.12.4.js"
+        integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+        crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
